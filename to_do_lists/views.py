2d616c2708b28df0ba1feab910ffc166ef244f4d
gustafelippe <gustavo.gugu77761@gmail.com>
2026-08-23 14:31:21 -0400

    Estelização da pagina index

--- to_do_lists/views.py
+++ to_do_lists/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from .forms import TarefaForm
+from .models import Tarefa
+
 
 def index(request):
     
-    form = TarefaForm()
-    context = {'form': form}
-    return render(request, 'to_do_lists/index.html', context)
+    if request.user.is_authenticated:
+        tarefas_total = Tarefa.objects.filter(user=request.user)
+        
+        if tarefas_total is not None:
+            tarefas_concluidas =[]
+            tarefas_nao_concluidas = []
+            
+            for tarefa in tarefas_total:
+                if tarefa.concluido == True:
+                    tarefas_concluidas.append(tarefa)
+                else:
+                    tarefas_nao_concluidas.append(tarefa)
+        else:
+            tarefas_concluidas = None
+            tarefas_nao_concluidas = None
+            
+            
+        form = TarefaForm()
+        context = {'form': form, 
+                   'tarefas_concluidas': tarefas_concluidas, 
+                   'tarefas_nao_concluidas': tarefas_nao_concluidas
+                   }
+        return render(request, 'to_do_lists/index.html', context)
+
+    else:
+        return render(request, 'to_do_lists/index.html')
