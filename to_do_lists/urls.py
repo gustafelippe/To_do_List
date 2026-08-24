6a940a2de858abc7630ba6f4b6503a2ee6dfb91d
gustafelippe <gustavo.gugu77761@gmail.com>
2026-08-23 20:21:28 -0400

    feat: finalizando cadastros

--- to_do_lists/urls.py
+++ to_do_lists/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name= 'index'),
+    path('concluir/<int:tarefa_id>/', views.concluir, name='concluir'),
+    path('excluir/<int:tarefa_id>/', views.excluir, name='excluir'),
 ]
--- to_do_lists/views.py
+++ to_do_lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TarefaForm
 from .models import Tarefa
 
@@ -21,7 +21,24 @@ def index(request):
             tarefas_concluidas = None
             tarefas_nao_concluidas = None
             
-            
+        
+        if request.method == 'POST':
+            form = TarefaForm(request.POST)
+            if form.is_valid():
+                user = request.user
+                tarefa = form.cleaned_data['tarefa']
+                concluido = False
+                new_tarefa = Tarefa.objects.create(user=user, tarefa=tarefa, concluido=concluido)
+                new_tarefa.save()
+                
+                return redirect('index') 
+            else:
+                context = {'form': form, 
+                            'tarefas_concluidas': tarefas_concluidas, 
+                            'tarefas_nao_concluidas': tarefas_nao_concluidas
+                    }
+                return render(request, 'to_do_lists/index.html', context)
+        
         form = TarefaForm()
         context = {'form': form, 
                    'tarefas_concluidas': tarefas_concluidas, 
@@ -31,3 +48,33 @@ def index(request):
 
     else:
         return render(request, 'to_do_lists/index.html')
+    
+def concluir(request, tarefa_id):
+    if request.user.is_authenticated:
+        tarefas_total = Tarefa.objects.filter(user=request.user)
+                
+        if tarefas_total is not None:
+           
+            for tarefa in tarefas_total:
+                if tarefa.id == tarefa_id:
+                    tarefa.concluido = True
+                    tarefa.save()
+                    
+                    return redirect('index')
+                
+    else:
+        return redirect('index')
+
+def excluir(request, tarefa_id):
+    if request.user.is_authenticated:
+        tarefas_total = Tarefa.objects.filter(user=request.user)
+                    
+        if tarefas_total is not None:
+               
+            for tarefa in tarefas_total:
+                if tarefa.id == tarefa_id:
+                    tarefa.delete()
+                    return redirect('index')
+                    
+    else:
+        return redirect('index')
